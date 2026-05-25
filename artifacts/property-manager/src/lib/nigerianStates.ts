99dbf22d7df37619eaac9d6d75475f8be1ab8342
livarexhomes <livarexhomes@gmail.com>
2026-05-25 15:38:47 +0000

    feat: restrict location options to Lagos and Ogun only

--- artifacts/property-manager/src/lib/nigerianStates.ts
+++ artifacts/property-manager/src/lib/nigerianStates.ts
@@ -1,21 +1,9 @@
 export const NIGERIAN_STATES = [
-  'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa',
-  'Benue', 'Borno', 'Cross River', 'Delta', 'Ebonyi', 'Edo', 'Ekiti',
-  'Enugu', 'FCT (Abuja)', 'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano',
-  'Katsina', 'Kebbi', 'Kogi', 'Kwara', 'Lagos', 'Nasarawa', 'Niger',
-  'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto',
-  'Taraba', 'Yobe', 'Zamfara',
+  'Lagos',
+  'Ogun',
 ]
 
 export const POPULAR_AREAS: Record<string, string[]> = {
   'Lagos': ['Lekki', 'Victoria Island', 'Ikeja', 'Surulere', 'Ikorodu', 'Ajah', 'Yaba', 'Gbagada', 'Magodo', 'Maryland', 'Ojodu', 'Badagry', 'Epe', 'Lagos Island', 'Apapa'],
-  'FCT (Abuja)': ['Maitama', 'Asokoro', 'Wuse', 'Garki', 'Gwarinpa', 'Jabi', 'Kado', 'Katampe', 'Kubwa', 'Life Camp', 'Lugbe', 'Utako', 'Wuse 2', 'Central Area'],
-  'Rivers': ['Port Harcourt', 'GRA Phase 1', 'GRA Phase 2', 'D-Line', 'Rumuola', 'Rumuola', 'Trans Amadi', 'Oyigbo', 'Obio-Akpor'],
-  'Oyo': ['Ibadan', 'Bodija', 'Challenge', 'Dugbe', 'Jericho', 'New Bodija', 'Ring Road', 'Oluyole', 'Iyaganku'],
-  'Kano': ['Kano City', 'Nasarawa GRA', 'Bompai', 'Fagge', 'Gwale', 'Tarauni', 'Dala'],
-  'Delta': ['Warri', 'Asaba', 'Effurun', 'Sapele', 'Ughelli'],
-  'Anambra': ['Awka', 'Onitsha', 'Nnewi', 'Ekwulobia'],
-  'Enugu': ['Enugu GRA', 'Independence Layout', 'New Haven', 'Trans Ekulu'],
-  'Edo': ['Benin City', 'GRA Benin', 'Ugbowo', 'Sapele Road'],
-  'Kaduna': ['Kaduna North', 'Kaduna South', 'Barnawa', 'Malali'],
+  'Ogun': ['Abeokuta', 'Sagamu', 'Ijebu-Ode', 'Ota', 'Ilaro', 'Ewekoro', 'Ifo', 'Obafemi-Owode', 'Remo North'],
 }
